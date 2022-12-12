6aba8d89842cdbd87b2eb05c49ac2084a23ceed3
Joanaescar <joanaa-esteves@hotmail.com>
2022-12-12 17:16:18 +0000

    Create new folder for routing

---
+++ routing/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    ServerComponent,
+    EditServerComponent,
+    UsersComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
