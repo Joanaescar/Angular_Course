0f311865c73f33451fb426f3adb0dacd53c58bb8
Joanaescar <joanaa-esteves@hotmail.com>
2022-12-20 11:41:25 +0000

    Done the assignment proposed in the course

--- basics-assignment-6/src/app/app.component.ts
+++ basics-assignment-6/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basics-assignment-6';
+  @ViewChild('form') signupForm!: NgForm;
+  user = {
+    email: '',
+    option: '',
+    password: ''
+  };
+  submitted: boolean = false;
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.email = this.signupForm.form.value.userData.email;
+    this.user.option = this.signupForm.form.value.userData.subscriptions;
+    this.user.password = this.signupForm.form.value.userData.password;
+
+    this.signupForm.reset()
+  }
 }
